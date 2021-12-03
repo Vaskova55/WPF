2adba173441b408f505ad73d434158b03e8416f1
uservsuservs <uservsuservs@yandex.ru>
2021-12-03 16:13:21 +0400

    Add project files.

---
+++ Vaskova-433/Classes/APIClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Vaskova_433.Classes.Elements;
+
+namespace Vaskova_433.Classes
+{
+    class APIClass
+    {
+        public static List<InquiryClass> Vivod()
+        {
+            HttpWebRequest webRequest = (HttpWebRequest)HttpWebRequest.Create ("https://api.cfif31.ru/BookRequest");
+            DataContractJsonSerializer contractJsonSerializer = new DataContractJsonSerializer(typeof (List<InquiryClass>));
+            List<InquiryClass> inquiryList = (List<InquiryClass>)contractJsonSerializer.ReadObject(webRequest.GetResponse().GetResponseStream());
+            return inquiryList;
+        }
+    }
+}
---
+++ Vaskova-433/Classes/Elements/InquiryClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vaskova_433.Classes.Elements
+{
+    [DataContract]
+    class InquiryClass
+    {
+        public enum status_inquiry : sbyte
+        {
+            rejected,
+            in_processing,
+            done
+        };
+        [DataMember]
+        [Key]
+        public int id { get; set; }
+        [DataMember]
+        [Required, Column(TypeName = "Date"), DataType(DataType.Date)]
+        public string date { get; set; }
+        [DataMember]
+        public int classTrainess { get; set; }
+        [DataMember]
+        [Required, MaxLength(32)]
+        public string firstName { get; set; }
+        [DataMember]
+        [Required, MaxLength(32)]
+        public string lastName { get; set; }
+        [DataMember]
+        [MaxLength(32)]
+        public string middleName { get; set; }
+        [DataMember]
+
+        [Required, MaxLength(1024)]
+        public string inquiry { get; set; }
+        [DataMember]
+        [Required]
+        public status_inquiry status { get; set; }
+    }
+}
